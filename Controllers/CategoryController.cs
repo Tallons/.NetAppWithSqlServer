a86f3b7e952cd144efc5ab5fb9dca69fd910cbe6
Tallons <Tallons27@yahoo.com>
2020-10-05 23:35:34 -0600

    DB connection

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers {
+    public class CategoryController : Controller {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db) {
+            _db = db;
+        }
+        public IActionResult Index() {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+        public IActionResult Create() {
+            return View();
+        }
+    }
+}
--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,11 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Models;
 
 namespace WebApplication1.Data {
     public class AppDbContext : DbContext {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
 
         }
+        public DbSet<Category> Category { get; set; }
     }
 }
